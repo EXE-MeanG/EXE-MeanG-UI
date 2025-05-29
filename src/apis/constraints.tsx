de74cc88951eef8583ed5d0d1f131ffdb240c108
VoHoangKiet <muoivo.051064@gmail.com>
2025-05-30 00:33:56 +0700

    fix build

--- src/apis/constraints.tsx
+++ src/apis/constraints.tsx
@@ -1,3 +1,3 @@
 // export const developmentURL = "https://raji-be-gamma.vercel.app/";
 // export const productionURL = ""
-export const developmentURL = "http://localhost:4000/";
+export const developmentURL = process.env.APP_URL;

bd1e5d99ed964c8919119ec0f2f10302b715c1c7
VoHoangKiet <muoivo.051064@gmail.com>
2025-06-06 00:24:28 +0700

    fix show avt

--- src/components/shared/AvaDIsc/discCustom.tsx
+++ src/components/shared/AvaDIsc/discCustom.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import Image from "next/image";
+import { Image } from "antd";
 import "./avatarDiscCustom.css";
 import { CameraOutlined } from "@ant-design/icons";
 
